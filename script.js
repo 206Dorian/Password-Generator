96415d64b6ca645f5056151b5b59c36d57d1ed8e
Dorian Birch <206dorian@gmail.com>
2022-08-28 21:58:04 -0500

    added so much to JS, Arrays, ifs, console.logs, etc, will resume tomorrow

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
+function generatePassword(passwordLength){
   console.log ("Hey!")
 
-var alphaLower = "abcdefghijklmnopqrstuvwxyz"
-var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numbers = "0123456789"
-var symbols = "!@#$%^&*+=" 
+  var userInput = window.prompt("How long do you want your password to be?")
 
-var options = [alphaLower, alphaUpper, numbers, symbols];
+var passwordLength = parseInt(userInput)
 
-prompt
+if (isNaN(passwordLength)){
+  window.alert("Not a Number")
+  return
+}
 
-window.confirm
+if (passwordLength < 8 || passwordLength > 128){
+  window.alert("password must be between 8 -128")
+  return
+}
+var wantsAlphaUpper = window.confirm("would you like to use Uppercase letters?")
+console.log("Yes Please Uppers")
 
-//Prompt the user for the password criteria
-//password lenght 8 to 128
-//lowercase, uppercase, numbers, special char 
+var wantsAlphaLower = window.confirm("would you like to use Lowercase letters?")
+console.log("Yes Please Lowers")
 
-// Validate the input  input validation
-//Generate password base don criteria
+var wantsNumbers = window.confirm("would you like to use Numbers?")
+console.log("Yes Please Numbers")
+
+var wantsSpecChar = window.confirm("would you like to use Special Characters?")
+console.log("Yes Please Special")
+
+
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var specChar = ["!", "@", "#", "$", "%", "&", "*", "_", "+", "="]
+
+var optionsCart = [alphaLower + alphaUpper + numbers + specChar];
+
+if (wantsAlphaUpper === true){
+optionsCart.push(alphaUpper)
+}
+
+if (wantsAlphaLower === true){
+  optionsCart.push(alphaLower)
+}
+
+if (wantsNumbers === true){
+  optionsCart.push(numbers)
+}
+
+if (wantsSpecChar === true){
+optionsCart.push(specChar)
+}
+
+var generatedPassword = ""
+
+for (var i = 0; i <= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1);
+ }
 
 
 //display generated password on page
@@ -42,7 +81,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-/*for (var i = 0; i <= passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random() * chars.length);
-  password += chars.substring(randomNumber, randomNumber +1);
- }*/
+
+
+
+
