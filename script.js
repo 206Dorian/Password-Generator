8274d30c0b2736b5f4529e51d02d147613225767
Dorian Birch <206dorian@gmail.com>
2022-08-29 14:32:51 -0500

    just backing up after a couple random changes, but dont want to lose anything

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function randomInt(min,max){}
+
 function generatePassword(passwordLength){
   console.log ("Hey!")
 
@@ -33,7 +35,9 @@ console.log("Yes Please Special")
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 var specChar = ["!", "@", "#", "$", "%", "&", "*", "_", "+", "="]
 
 var optionsCart = [alphaLower + alphaUpper + numbers + specChar];
@@ -54,12 +58,18 @@ if (wantsSpecChar === true){
 optionsCart.push(specChar)
 }
 
+console.log(optionsCart)
+
 var generatedPassword = ""
 
+
 for (var i = 0; i <= passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random() * chars.length);
-  password += chars.substring(randomNumber, randomNumber +1);
- }
+var randomItem = optionsCart[randomInt(0, optionscart.length -1)]
+generatePassword
+
+  //var randomNumber = Math.floor(Math.random() * chars.length);
+ // password += chars.substring(randomNumber, randomNumber +1);
+ //}
 
 
 //display generated password on page
